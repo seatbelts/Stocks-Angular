bf4c5248bc855533f1b08377a1600815c48f1be1
seatbelts <is.ma95@hotmail.com>
2023-12-19 23:13:38 -0600

    Added Logic to retrieve Data and Display it

--- src/app/stock/components/b/b.component.ts
+++ src/app/stock/components/b/b.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StockService } from '../../services/stock.service';
+import { Observable } from 'rxjs';
+import { StockInterface } from '../../../interfaces.stock/interfaces/stock';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-b',
   templateUrl: './b.component.html',
   styleUrl: './b.component.scss'
 })
-export class BComponent {
+export class BComponent implements OnInit {
+
+  public stockInformation$: Observable<StockInterface> | undefined;
+
+  constructor(
+    private router: Router,
+    private stockService: StockService
+  ) {}
+
+  ngOnInit(): void {
+    this.stockInformation$ = this.stockService.stockInformation();
+  }
+
+  navigateToA() {
+    this.router.navigate(['/stock/a']);
+  }
 
 }
