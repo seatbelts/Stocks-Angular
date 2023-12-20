759d9505e3ac98b19def5166604f66b3e890b7f8
seatbelts <is.ma95@hotmail.com>
2023-12-19 22:35:53 -0600

    Implemented Stock Algorithm

---
+++ src/app/interfaces.stock/interfaces/stock.ts
@@ -0,0 +1,5 @@
+export interface StockInterface {
+  maxProfit: number | string;
+  buyDay: number | string;
+  sellDay: number | string;
+}
--- src/app/stock/services/stock.service.ts
+++ src/app/stock/services/stock.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { StockInterface } from '../../interfaces.stock/interfaces/stock';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StockService {
 
+  private testCase: number[] = [25, 36, 78, 14, 64, 27, 80];
+
+  private maxProfit: number = 0;
+
+  private buyDay: number = 0;
+
+  private sellDay: number = 0;
+
   constructor() { }
+
+  public stockAlgorithm(stocks?: number[]): void {
+    stocks = stocks || this.testCase;
+    let minPrice = Math.max(...stocks);
+    this.maxProfit = 0;
+    for (let i = 0; i < stocks.length; i++) {
+      if (stocks[i] < minPrice) {
+        minPrice = stocks[i];
+        this.buyDay = i;
+      } else if (stocks[i] - minPrice > this.maxProfit) {
+        this.maxProfit = stocks[i] - minPrice;
+        this.sellDay = i;
+      }
+    }
+  }
+
+  public stockInformation(): Observable<StockInterface> {
+    return of({
+      maxProfit: this.maxProfit || 'None',
+      buyDay: this.maxProfit ? this.buyDay : 'None',
+      sellDay: this.maxProfit ? this.sellDay : 'None',
+    });
+  }
 }
