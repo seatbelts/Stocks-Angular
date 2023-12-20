3545dd0027a4af9d2c28c5c43fb63636870a6db4
seatbelts <is.ma95@hotmail.com>
2023-12-19 23:05:51 -0600

    Implemented navigation and call to algorithm in A Component

--- src/app/stock/components/a/a.component.ts
+++ src/app/stock/components/a/a.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { StockService } from '../../services/stock.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-a',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './a.component.scss'
 })
 export class AComponent {
+
+  constructor(
+    private stockService: StockService,
+    private router: Router
+  ) {}
+
+  navigateToB() {
+    this.stockService.stockAlgorithm();
+    this.router.navigate(['/stock/b']);
+  }
 
 }
